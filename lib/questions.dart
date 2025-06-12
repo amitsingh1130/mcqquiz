9dd0e9002320d07626d9e258a4eef16b36311eb1
amitsingh1130 <amits979563@gmail.com>
2025-06-12 16:31:20 +0530

    mcq quiz

--- lib/questions.dart
+++ lib/questions.dart
@@ -1,20 +1,51 @@
 import 'package:flutter/material.dart';
+
 class Question {
   String question, opta, optb, optc, optd;
   int correctanswer;
   Question(
-      this.question,
-      this.opta,
-      this.optb,
-      this.optc,
-      this.optd,
-      this.correctanswer,
-      );
+    this.question,
+    this.opta,
+    this.optb,
+    this.optc,
+    this.optd,
+    this.correctanswer,
+  );
 }
 
 class Utilities {
   static List<Question> getQuestions() {
     List<Question> questions = [
+      Question(
+        "Which programming language is used to develop apps in Flutter?",
+        "Kotlin",
+        "Dart",
+        "Swift",
+        "Java",
+        2,
+      ),
+      Question(
+        "In Flutter, what widget is used for layout in a vertical direction?",
+        "Row",
+        "Stack",
+        "Column",
+        "Expanded",
+        3,
+      ),
+      Question(
+        " Which command creates a new Flutter project from the terminal?",
+        "flutter create_project my_app",
+        "flutter run my_app",
+        "flutter create my_app",
+        "flutter init my_app",
+        3,
+      ),
+      Question(" What is the use of the Scaffold widget in Flutter?",
+          "For managing screen transitions",
+          "For creating lists",
+          "For dependency injection",
+          "For defining app structure like AppBar, Drawer, Body, etc.",
+          4),
       Question(
         "Which is the capital of japan",
         "Hukulganj",
