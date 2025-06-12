bdd9e121d1ab44e2d4ef739a7f4350945fe9adde
amitsingh1130 <amits979563@gmail.com>
2025-06-12 14:49:13 +0530

    Separated questions

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-
+import 'question.dart';
 void main() {
   runApp(VSJQuizApp());
 }
@@ -228,64 +228,3 @@ class VSJQuizState extends State<VSJQuiz> {
 }
 
 //********************************************************************
-
-class Question {
-  String question, opta, optb, optc, optd;
-  int correctanswer;
-  Question(
-    this.question,
-    this.opta,
-    this.optb,
-    this.optc,
-    this.optd,
-    this.correctanswer,
-  );
-}
-
-class Utilities {
-  static List<Question> getQuestions() {
-    List<Question> questions = [
-      Question(
-        "Which is the capital of japan",
-        "Hukulganj",
-        "Tokyo",
-        "Lucknow",
-        "China",
-        2,
-      ),
-      Question(
-        "Which is the Capital of China",
-        "US",
-        "UK",
-        "Chandigarh",
-        "Beijing",
-        4,
-      ),
-      Question(
-        "Which is The capital of India",
-        "Delhi",
-        "Bangluru",
-        "Patna",
-        "Asam",
-        1,
-      ),
-      Question(
-        "Which is The Capital of United Kingdom",
-        "Virginia",
-        "London",
-        "Washington",
-        "Canada",
-        2,
-      ),
-    ];
-
-    return questions;
-  }
-
-  static SnackBar getSnackBar(String message) {
-    final snackBar = SnackBar(content: Text(message));
-    return snackBar;
-  }
-}
-
-//****************************************************************
